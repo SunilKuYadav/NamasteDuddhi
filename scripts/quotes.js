3b3c827dcab6fb2c01954784094690b6dbe0c7d0
Sunil Kumar Yadav <mobcoderid-105@Mobcoders-Mac-mini-5.local>
2022-10-26 18:01:00 +0530

    Update: quotes

---
+++ scripts/quotes.js
@@ -0,0 +1,53 @@
+const DATA = [
+  "these are some most important and  precious advices every one should read  💜🙂",
+  "Take risks in your life. If you win, you can lead; if you lose, you can guide.",
+  "People are not what they say but what they do; so judge them not from their words but from their actions.",
+  "When someone hurts you, don't feel bad because it's a law of nature that the tree that bears the sweetest fruits gets maximum number of stones.",
+  "Take whatever you can from your life because when life starts taking from you, it takes even your last breath.",
+  "In this world, people will always throw stones on the path of your success. It depends on what you make from them - a wall or a bridge.",
+  "Challenges make life interesting; overcoming them make life meaningful.",
+  "There is no joy in victory without running the risk of defeat.",
+  "A path without obstacles leads nowhere.",
+  "Past is a nice place to visit but certainly not a good place to stay.",
+  "You can't have a better tomorrow if you are thinking about yesterday all the time.",
+  "If what you did yesterday still looks big to you, then you haven't done much today.",
+  "If you don't build your dreams, someone else will hire you to build theirs.",
+  "If you don't climb the mountain; you can't view the plain.",
+  "Don't leave it idle - use your brain.",
+  "You are not paid for having brain, you are only rewarded for using it intelligently.",
+  "It is not what you don't have that limits you; it is what you have but don't know how to use.",
+  "What you fail to learn might teach you a lesson.",
+  "The difference between a corrupt person and an honest person is: The corrupt person has a price while the honest person has a value.",
+  "If you succeed in cheating someone, don't think that the person is a fool...... Realize that the person trusted you much more than you deserved.",
+  "Honesty is an expensive gift; don't expect it from cheap people.",
+];
+
+let counter = 0;
+const length = DATA.length - 1;
+
+// function for moving left and right
+const move = (direction) => {
+  document.getElementById(`${counter}`).style.display = "none";
+  if (counter < length && direction) {
+    document.getElementById(`${counter + 1}`).style.display = "flex";
+    counter += 1;
+    return null;
+  }
+  if (counter === length && direction) {
+    document.getElementById(`${0}`).style.display = "flex";
+    counter = 0;
+    return null;
+  }
+  if (counter === 0 && !direction) {
+    document.getElementById(`${length}`).style.display = "flex";
+    counter = length;
+    return null;
+  }
+  document.getElementById(`${counter - 1}`).style.display = "flex";
+  counter -= 1;
+  return null;
+};
+
+const showQuoteData = () => {
+  document.getElementById(`${counter}`).style.display = "flex";
+};
